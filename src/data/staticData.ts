f53baeaff609fbefa65cc43aaed30b4a6f7c7a00
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-06-27 12:28:15 -0300

    create simple homepage

--- src/data/staticData.ts
+++ src/data/staticData.ts
@@ -20,5 +20,39 @@ export const flags:flagProps[] = [
     flagName:"United States",
     flagUrl:"/flags/us.svg",
     flagPageUrl:"/humans/unitedstates/"
+   },
+   {
+    flagId:3,
+    flagAbreviation:"de",
+    flagName:"Germany",
+    flagUrl:"/flags/Germany.svg",
+    flagPageUrl:"/humans/germany/"
+   },
+   {
+    flagId:3,
+    flagAbreviation:"es",
+    flagName:"Spain",
+    flagUrl:"/flags/Spain.svg",
+    flagPageUrl:"/humans/spain/"
+   },
+   {
+    flagId:3,
+    flagAbreviation:"de",
+    flagName:"France",
+    flagUrl:"/flags/France.svg",
+    flagPageUrl:"/humans/france/"
+   },
+   {
+    flagId:3,
+    flagAbreviation:"gb",
+    flagName:"United Kingdom",
+    flagUrl:"/flags/United_Kingdom.svg",
+    flagPageUrl:"/humans/unitedkingdom/"
    }
 ]
+
+
+// {flagId:7,flagAbreviation:"es",flagName:"Spain",flagAdreess:"/flags/Spain.svg",flagHref:"/nations/nation/es/"},
+// {flagId:8,flagAbreviation:"fr",flagName:"France",flagAdreess:"/flags/France.svg",flagHref:"/nations/nation/fr/"},
+// {flagId:9,flagAbreviation:"gb",flagName:"United Kingdom",flagAdreess:"/flags/United_Kingdom.svg",flagHref:"/nations/nation/gb/"},  
+
