2b55e03c625692a51d1c184b6d2495ab1c75b589
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-06-27 02:07:04 -0300

    Update header.tsx

--- src/components/layoult/header/header.tsx
+++ src/components/layoult/header/header.tsx
@@ -2,8 +2,6 @@ import HeaderRoot from "./headerRoot";
 import HeaderBrand from "./headerBrand";
 import HeaderLinks from "./headerLinks";
 
-
-
 export const Header = {
     root:HeaderRoot,
     brand:HeaderBrand,
