b62175ea0d60b15fca8c30283a87569b5cdeeeeb
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-06-27 18:22:48 -0300

    v1.0  - change header git hub url

--- src/components/layoult/header/headerLinks.tsx
+++ src/components/layoult/header/headerLinks.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const HeaderLinks = ()=>{
     return(
         <nav className="flex gap-3 justify-start items-center text-white duration-300">
-            <Link href={"https://github.com/PetrusNoleto/fakehumandatabase"} className="hover:text-[#FF5C00] capitalize" target="_blank">github</Link>
+            <Link href={"https://github.com/PetrusNoleto/fakehumandatabaseweb"} className="hover:text-[#FF5C00] capitalize" target="_blank">github</Link>
         </nav>
     )
 }
